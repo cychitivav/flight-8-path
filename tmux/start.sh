b53c01e10daea6dd3f8f766db44727ad0e23f326
Cristian Chitiva <cychitivav@unal.edu.co>
2024-09-29 20:56:18 +0000

    Add tmux configurations

---
+++ tmux/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=$(dirname $SCRIPT)
+cd "$SCRIPTPATH"
+
+export TMUX_SESSION_NAME=simulation
+export TMUX_SOCKET_NAME=mrs
+
+# start tmuxinator
+tmuxinator start -p ./session.yml
+
+# if we are not in tmux
+if [ -z $TMUX ]; then
+  tmux -L $TMUX_SOCKET_NAME a -t $TMUX_SESSION_NAME # just attach to the session
+else
+  # switch to the newly-started session
+  tmux detach-client -E "tmux -L $TMUX_SOCKET_NAME a -t $TMUX_SESSION_NAME"
+fi
